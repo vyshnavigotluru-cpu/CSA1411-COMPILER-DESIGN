2fde411f546e0df7100cc0f42616046f19524061
vyshnavigotluru-cpu <vyshnavigotluru@gmail.com>
2026-08-15 09:36:37 +0530

    Create exp.c

---
+++ exp18/exp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("TRAILING SETS\n");
+    printf("=============\n");
+
+    printf("TRAILING(E) = { +, ), id }\n");
+    printf("TRAILING(T) = { *, +, ), id }\n");
+    printf("TRAILING(F) = { ), id, * }\n");
+
+    return 0;
+}
