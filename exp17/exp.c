3116e4bfedc1320685e4711edc97aa28cb14f424
vyshnavigotluru-cpu <vyshnavigotluru@gmail.com>
2026-08-15 09:34:27 +0530

    Create exp.c

---
+++ exp17/exp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("LEADING SETS\n");
+    printf("============\n");
+
+    printf("LEADING(E) = { +, *, (, id }\n");
+    printf("LEADING(T) = { *, (, id }\n");
+    printf("LEADING(F) = { (, id }\n");
+
+    return 0;
+}
