4f1017ef529bb7c54c571ef80b471568f8795dfa
vyshnavigotluru-cpu <vyshnavigotluru@gmail.com>
2026-08-15 08:45:06 +0530

    Create EXP.C

---
+++ EXP8/EXP.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("FOLLOW SETS\n");
+    printf("============\n");
+
+    printf("FOLLOW(S) = { $ }\n");
+    printf("FOLLOW(A) = { a, b }\n");
+    printf("FOLLOW(B) = { a, b }\n");
+
+    return 0;
+}
